ce547b0dfb4f1aa7625bcc98ccfdcca7c1044676
Kishan Silawat <kishansilawat@gmail.com>
2019-11-25 23:17:46 +0530

    feat: Created Post model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length=20)
+	content = models.TextField(max_length=200)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
