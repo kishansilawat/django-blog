1c749bdcba7ab693ab8cd72b181f987162a5e53c
Kishan Silawat <kishansilawat@gmail.com>
2019-11-25 23:54:31 +0530

    feat: Added Post model to admin

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+
+	list_display = (
+				'title',
+				'content',
+				'created',
+				'updated',
+				)
+
+	list_display_links = (
+					'title',
+					'created',
+					'updated',
+					)
+
+	search_fields = (
+				'title',
+				'content',
+				)
+
+	list_filter = (
+				'updated',
+				'created',
+				)
+
+admin.site.register(Post, PostAdmin)
